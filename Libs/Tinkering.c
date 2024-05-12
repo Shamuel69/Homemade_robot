a429e5c04e635ca77446671cd3aaf2a625545c57
Samuel the Ultimate <tobbanganmantis@gmail.com>
2024-05-12 19:54:42 -0400

    main

---
+++ Libs/Tinkering.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void display(char* str, int age) {
+    printf("My name is %s");
+}
--- Libs/grabjson.py
+++ Libs/grabjson.py
@@ -1,5 +1,14 @@
 import os
 import ctypes
+#C:\Users\tobba\programming folder\python\FixingAverage\CarlBot2-Github\Libs\Tinkering.c
+
+clibrary = ctypes.CDLL(r"C:\Users\tobba\programming folder\python\FixingAverage\CarlBot2-Github\Libs/cTinkering.so")
+
+
 
 def JSON_grab():
-    pass
+    clibrary.display()
+
+
+if __name__ == "__main__":
+    JSON_grab
